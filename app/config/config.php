e8c6a90c0d0cc78ebc4bd05d6b26f51b7617f2bd
dwi sugi <dwisugianto17@gmail.com>
2019-09-05 16:53:37 +0700

    add database

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/phpmvc/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'sugi');
+define('DB_PASS', 'Qwerty123$');
+define('DB_NAME', 'phpmvc');
