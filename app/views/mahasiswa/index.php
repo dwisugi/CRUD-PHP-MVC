9acdedd7cff1cfb8dd6bc300a6068a8c592b3e1b
dwi sugi <dwisugianto17@gmail.com>
2019-09-06 17:28:43 +0700

    add delete and update

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,14 @@
 <div class="container mt-3">
-    <div class="col-6">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+
+    <div class="row">
+      <div class="col-lg-6">
+        <?php Flasher::flash();?>
+      </div>
+    </div>
+
+
+    <div class="col-lg-6">
+        <button type="button" class="btn btn-primary tombolTambahData tampilModalTambah" data-toggle="modal" data-target="#formModal">
             Tambah Data Mahasiswa
         </button>
         <br><br>
@@ -8,9 +16,11 @@
         
         <ul class="list-group">
             <?php foreach ($data['mhs'] as $mhs) : ?>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
+                <li class="list-group-item">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-primary ">Detail</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'];?>" class="badge badge-danger float-right ml-1" onclick="return confirm('Yakin?');">Hapus</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-id="<?= $mhs['id']; ?>" data-target="#formModal">Ubah</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-primary float-right ml-1">Detail</a>
                 </li>
             <?php endforeach; ?>
         </ul>
@@ -19,17 +29,18 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+        <input type="hidden" name="id" id="id">
           <div class="form-group">
             <label for="nama">Nama</label>
             <input type="text" class="form-control" id="nama" name="nama">
@@ -57,7 +68,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        <button type="submit" class="btn btn-primary" id="formModalLabel">Tambah Data</button>
         </form>
       </div>
     </div>
