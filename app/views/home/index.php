141862c9b810953792073f38a27c2ad0ca9b2ecc
dwisugi <dwisugianto17@gmial.com>
2019-11-09 14:40:59 +0700

    modified home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
    <div class="jumbotron mt-5">
-      <h1 class="display-4">Selamat Datang, di Website Saya</h1>
-      <p class="lead">Hallo, nama saya <?= $data['nama']?></p>
+      <h1 class="display-4">Welcome in my Website</h1>
+      <p class="lead">Hello, my name is <?= $data['nama']?></p>
       <hr class="my-4">
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
