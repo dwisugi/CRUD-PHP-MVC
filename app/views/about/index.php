b950c0a7e92bb4a92921910778a64e71a3902b9b
dozennium <56032054+dozennium@users.noreply.github.com>
2020-10-02 21:14:10 +0700

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
     <h1 class="mt-5">About Me</h1>
     <img src="<?= BASEURL; ?>/img/jago.jpg" alt="Ayam Jago" width="200" class="rounded shadow">
-    <p>Hallo,nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?>, saya adalah <?= $data['pekerjaan']?>.</p>
+    <p>Hallo,Nama saya <?= $data['nama']?>, Umur saya <?= $data['umur']?>, Saya adalah <?= $data['pekerjaan']?>.</p>
 </div>
